b6b57e082f7537216963c740bb7af630a4f93229
Anshika070503 <anshikasinghal161@gmail.com>
2025-02-07 10:13:04 +0530

    importing done

---
+++ back/fileSystem/filecallback.js
@@ -0,0 +1,14 @@
+
+//asynchronous(non-blocking nature)
+const read = ()=>{
+    const fs = require("fs");
+    fs.readFile("./data.txt","utf-8",(error,data)=>{
+        if(error){
+            console.error("Error reading file: ",error);
+        }
+        else{
+            console.log("Data from file:",data);
+        }
+    });
+}
+module.exports = read;
---
+++ back/fileSystem/fileimport.js
@@ -0,0 +1,5 @@
+
+const read = require("./filecallback.js");
+read();
+console.log("complete me first");
+read();
